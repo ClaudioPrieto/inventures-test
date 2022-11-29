b2397889daa5edb6c1136f2b087d54846912578d
ClaudioPrieto <cjprieto@uc.cl>
2022-11-29 13:25:59 -0300

    add solution with parenthesis

--- logic/helpers.py
+++ logic/helpers.py
@@ -1,8 +1,11 @@
 from itertools import permutations, combinations_with_replacement
 
 def printSolutions(solutions: list):
-    for solution in solutions:
-        print(solution)
+    if solutions:
+        for solution in solutions:
+            print(solution) 
+    else:
+        print("No Solution")
 
 def getAllUniqueCombinations(digitsQuantity: int):
     operations = ['+', '-', '*', '/']
@@ -12,8 +15,22 @@ def getAllUniqueCombinations(digitsQuantity: int):
         for oper in permutations(operationsCombination):
             if oper not in uniqueOperations:
                 uniqueOperations.append(oper)
-    
+
     return uniqueOperations
 
 def generateFormula(values: set, operations: set) -> str:
     return '{}{}{}{}{}{}{}'.format(values[0], operations[0], values[1], operations[1], values[2], operations[2], values[3])
+
+def generateParenthesisFormula(values: set, operations: set, parentesis: str) -> str:
+    currentOperation = 0
+    currentValue = 0
+    formulaList = list(parentesis)
+    for i in range(len(parentesis)):
+        if formulaList[i] == '$':
+            formulaList[i] = operations[currentOperation]
+            currentOperation += 1
+        elif formulaList[i] == 'x':
+            formulaList[i] = values[currentValue]
+            currentValue += 1
+
+    return ''.join(formulaList)
--- logic/main.py
+++ logic/main.py
@@ -1,37 +1,62 @@
 from itertools import permutations
-from helpers import getAllUniqueCombinations, printSolutions, generateFormula
+from helpers import getAllUniqueCombinations, printSolutions, generateFormula, generateParenthesisFormula
 
 def compute25(numbers: str, target: int) -> list:
-
     numbersArray = list(numbers)
+    operationsCombinations = getAllUniqueCombinations(len(numbersArray) - 1)
     solutions = []
     
-    for operationsCombination in getAllUniqueCombinations(len(numbersArray) - 1):
-            currentFormula = generateFormula(numbersArray, operationsCombination)
+    for combination in operationsCombinations:
+            currentFormula = generateFormula(numbersArray, combination)
             if currentFormula not in solutions and eval(currentFormula) == target:
                 solutions.append(currentFormula)
     
     return solutions
                 
 def compute25NoOrder(numbers: str, target: int) -> list:
-    
-    numbersArray = list(numbers)  
+    numbersArray = list(numbers)
+    operationsCombinations = getAllUniqueCombinations(len(numbersArray) - 1)
     solutions = []
 
     for values in permutations(numbersArray):
-        for operationsCombination in getAllUniqueCombinations(len(numbersArray) - 1):
-            currentFormula = generateFormula(values, operationsCombination)
-            if currentFormula not in solutions and eval(currentFormula) == target:
-                solutions.append(currentFormula)
+        for combination in operationsCombinations:
+            currentFormula = generateFormula(values, combination)
+            try:
+                evaluation = eval(currentFormula)
+                if currentFormula not in solutions and evaluation == target:
+                    solutions.append(currentFormula)
+            except ZeroDivisionError:
+                print ("WARNING: Invalid Equation")
+    
+    return solutions
+ 
+def compute25Parenthesis(numbers: str, target: int) -> list:
+    numbersArray = list(numbers)
+    parenthesisCombinations = ['x$x$x$x', '(x$x)$x$x', 'x$(x$x)$x', 'x$x$(x$x)', '(x$x)$(x$x)', '(x$x$x)$x', 'x$(x$x$x)', '((x$x)$x)$x', '(x$(x$x))$x', 'x$((x$x)$x)', 'x$(x$(x$x))']
+    operationsCombinations = getAllUniqueCombinations(len(numbersArray) - 1)
+    solutions = []
+    
+    for values in permutations(numbersArray):
+        for operCombination in operationsCombinations:
+            for parenthCombination in parenthesisCombinations:
+                currentFormula = generateParenthesisFormula(values, operCombination, parenthCombination)
+                try:
+                    evaluation = eval(currentFormula)
+                    if currentFormula not in solutions and evaluation == target:
+                        solutions.append(currentFormula)
+                except ZeroDivisionError:
+                    print ("WARNING: Invalid Equation")
     
     return solutions
  
 if __name__ == "__main__":
     
-    input = "3671"
+    input = "8251"
     
     printSolutions(compute25(input, 25))
     print('-------')
     printSolutions(compute25NoOrder(input, 25))
+    print('-------')
+    printSolutions(compute25Parenthesis(input, 25))
     
     
